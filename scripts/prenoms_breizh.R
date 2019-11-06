3e64867004dec0297bd2a56acd750fa8938642fd
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-11-06 13:40:34 +0100

    prenom breizh

---
+++ scripts/prenoms_breizh.R
@@ -0,0 +1,9 @@
+###############################################################################
+## load libraries
+library(ggplot2)
+library(gganimate)
+library(gifski)
+library(tidyverse)
+library(RColorBrewer)
+library(png)
+
