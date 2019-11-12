f4a5f571269ffff37c9e7fd1f6354dca4b60784c
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-11-12 16:41:51 +0100

    co2 emissions scripts

---
+++ scripts/country_co2.R
@@ -0,0 +1,124 @@
+#https://static1.squarespace.com/static/5ae60dae4611a0353c4f5e8e/t/5b28e4b203ce64bc17771674/1529406658936/20141209_EPI-RF+Report+on+Demand+Side+Initiatives+FINAL.compressed.pdf
+
+###############################################################################
+## load libraries
+library(ggplot2)
+library(gganimate)
+library(gifski)
+library(tidyverse)
+library(RColorBrewer)
+library(png)
+library(extrafont)
+library(tweenr)
+library(ggflags)
+
+
+
+###############################################################################
+## functions
+
+interpolate_df <- function(madf,kept_frames,fpy,nb_year) {
+   filled_df <- madf[1,] %>% 
+  keep_state(kept_frames) %>% 
+  tween_state(madf[2,], ease = 'cubic-in-out', nframes = fpy*nb_year)  %>% 
+  keep_state(kept_frames)
+  return(filled_df)
+}
+
+
+###############################################################################
+## load data
+carbone=read.csv("donnees/co2_nations_format.csv",header=T,sep=",")
+names(carbone)=c("nation","year","fuel","solid","liquid","gas","cement","flaring","capita","bunker")
+carbone$fuel=carbone$fuel*1000
+
+
+
+myframe=40
+mykeptframe=1
+carbone_all= NULL
+for(pays in unique(carbone$nation)) {
+	current_p= carbone[which(carbone$nation ==pays),]
+	current_frame=0
+	for(y in 1:(length(unique(current_p$year))-1)) {	
+		current_p_y=current_p[c(y,y+1),]
+		interpol_curp=interpolate_df(current_p_y,mykeptframe,myframe,1)
+		interpol_curp$.frame=interpol_curp$.frame+current_frame
+		current_frame=current_frame+myframe+mykeptframe*2
+	}
+	carbone_all=rbind(carbone_all,interpol_curp)
+}
+
+carbone_all$year=as.integer(carbone_all$year)
+
+carbone_all$fuel=as.integer(carbone_all$fuel)
+
+carbone_s=carbone_all
+
+carbone_all$nation = fct_explicit_na(carbone_all$nation, na_level = "(Missing)")
+
+
+
+
+toplimit=12
+tops_format <- carbone_all %>%
+  group_by(.frame) %>%
+  mutate(rank = rank(-fuel,ties.method="first"),
+         Value_rel = fuel/fuel[rank==1],
+         Value_lbl = paste0(" ",fuel)) %>%
+  group_by(nation) %>% 
+  filter(rank <=toplimit) %>%
+  ungroup()
+
+###############################################################################
+## visualization
+nb.cols <- length(unique(tops_format$nation))
+africa_palette_color=c("#c35b48","#e5c027","#458962","#fa993e")
+mycolors <- sample(colorRampPalette(africa_palette_color)(nb.cols))
+
+
+
+staticplot = ggplot(tops_format, aes(-rank, group = nation,
+                                     fill = as.factor(nation), color = as.factor(nation))) +
+  geom_tile(aes(y = fuel/2,height = fuel, width = 0.9), alpha = 0.8, color = NA) +
+  geom_text(aes(y = fuel, label = paste(nation, "    ")),colour="gray10", hjust = 1,size=12) +
+  geom_text(aes(y=fuel,label = paste("   ",Value_lbl), hjust=0), colour="gray10",size=12) +
+  #geom_flag(aes(y=fuel),size=36)+
+  geom_text(aes(x=-7,y=Inf,label = "rejet co2", hjust=0.5,vjust=0.5), colour="black",fontface="bold",size=24) +
+  geom_text(aes(x=-8.5,y=Inf,label = "(en tonnes CO²)", hjust=0.5,vjust=0.5), colour="gray50",fontface="bold",size=18) +
+
+  geom_text(aes(x=-10,y=Inf,label = year, hjust=0.5,vjust=0.5), colour="gray50",fontface="bold",size=60) +
+  
+  coord_flip(clip = "off", expand = FALSE) +
+  scale_x_continuous(labels = scales::comma,position="bottom") +
+  scale_fill_manual(values=mycolors) +
+  guides(color = FALSE, fill = FALSE) +
+  theme(axis.line=element_blank(),
+        axis.text.x=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.position="none",
+        panel.background=element_blank(),
+        panel.border=element_blank(),
+        panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.grid.major.x = element_line( size=.1, color="grey" ),
+        panel.grid.minor.x = element_blank(),
+        plot.title=element_text(size=25, hjust=0.5, face="bold", colour="grey50", vjust=-1),
+        plot.subtitle=element_text(size=18, hjust=0.5, face="italic", color="grey50"),
+        plot.caption =element_text(size=18, hjust=0.5, face="italic", color="grey50"),
+        plot.background=element_blank(),
+        plot.margin = margin(2,12, 2, 6, "cm"))
+
+
+anim = staticplot +transition_manual(.frame) +
+    ease_aes('quadratic-in-out')+
+    view_follow(fixed_x = TRUE)
+ 
+
+
+nombre_frames=as.integer(dim(tops_format)[1]/toplimit)
+animate(anim, nframes=1000, fps = 40,  width = 1920, height = 1080,renderer = ffmpeg_renderer()) -> for_mp4
+anim_save("animation.mp4", animation = for_mp4 )
--- scripts/format_csv.sh
+++ scripts/format_csv.sh
@@ -1,6 +1,7 @@
 tr '[:upper:]' '[:lower:]' < nat2018.csv > nat2018_low.csv
 sed s/É/e/g nat2018_low.csv | sed s/Ï/i/g | sed s/Ü/u/g |sed s/Ö/o/g | sed s/Ë/e/g | sed s/È/e/g | sed s/ç/c/g | sed s/xxxx/XXXX/g > nat2018_anglo.csv
 
+
 awk 'BEGIN{OFS=FS=";"}
      NR>1{$2 = tolower($2);
           split($2,arr," ");
@@ -14,3 +15,15 @@ awk 'BEGIN{OFS=FS=";"}
           for(x in arr)
               sub(arr[x],toupper(substr(arr[x],1,1))substr(arr[x],2),$2)
          }1' nat2018_low.csv > nat2018_format.csv
+
+
+
+#######
+## co2
+
+tr '[:upper:]' '[:lower:]' < co2_nations.csv > co2_nations_low.csv
+
+
+
+
+sed 's/\w/\u&/' co2_nations_low.csv > co2_nations_format.csv
